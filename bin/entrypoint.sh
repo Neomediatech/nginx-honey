e1004a4bb6c0f0aba41ba05b604dce833f45abe8
Dario B <feddar@users.noreply.github.com>
2020-01-26 17:07:30 +0100

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -32,5 +32,24 @@ if [ -d "${SRC_DIR}" ]; then
   done
 fi
 
+# Check custom snippets
+SRC_DIR="/snippets"
+DST_DIR="/etc/nginx/snippets"
+if [ -d "${SRC_DIR}" ]; then
+  cd "${SRC_DIR}"
+  for FILE in $(find . -type f|cut -b 3-); do
+    DIR_FILE="$(dirname "$FILE")"
+    if [ ! -d "$DST_DIR/$DIR_FILE" ]; then
+      mkdir -p "$DST_DIR/$DIR_FILE"
+    fi
+    if [ -f "$DST_DIR/$FILE}" ]; then
+      echo "  WARNING: $DST_DIR/$FILE already exists and will be overriden"
+      rm -f "$DST_DIR/$FILE"
+    fi
+    echo "  Add custom config file $DST_DIR/$FILE ..."
+    ln -sf "$SRC_DIR/$FILE" "$DST_DIR/$FILE"
+  done
+fi
+
 exec tail -F ${LOGFILE} &
 exec "$@"
